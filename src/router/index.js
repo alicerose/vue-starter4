b0128600693921c2725009239764e21813029c95
Takehiro Kato <czk@seeds-9.com>
2019-12-20 01:18:05 +0900

    add router position

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,18 @@ Vue.use(VueRouter);
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
+  scrollBehavior(to, from, position) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        if (position) {
+          resolve(position);
+        } else {
+          resolve({ x: 0, y: 0 });
+        }
+      }, 500);
+    });
+  }
 });
 
 export default router;
