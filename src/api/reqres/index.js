8cf826a1698980c76de97d3dd872364bcbf4a9dd
Takehiro Kato <czk@seeds-9.com>
2019-12-20 03:58:32 +0900

    add get method sample

--- src/api/reqres/index.js
+++ src/api/reqres/index.js
@@ -7,12 +7,23 @@ import api from "@/api";
 export const Reqres = new (class ReqresApi extends api {
   HOST = "https://reqres.in/api/";
   USERS = "users";
+  USER = "users/";
 
   /**
-   * GET
-   * @returns {*}
+   * User list
+   * @param params
+   * @returns {Promise<unknown>}
    */
   getUserList(params) {
     return super.get(this.HOST, this.USERS, params);
+  }
+
+  /**
+   * User
+   * @param params
+   * @returns {Promise<unknown>}
+   */
+  getUser(params) {
+    return super.get(this.HOST, this.USER + params);
   }
 })();
